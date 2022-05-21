0d0b35086e868cc2ed827afd58e343571ad31f7f
Oguz Kaan C Kilinc <okckilinc@gmail.com>
2022-05-21 13:16:58 +0300

    rendering

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('Hello World');
+
+function run() {
+    console.log('Running');
+
+    let codeHTML  = document.getElementById('codeHTML').value;
+    let codeJS    = document.getElementById('codeJS').value;
+    let codeCSS   = '<style>' + document.getElementById('codeCSS').value + '</style>';
+    let runCode   = document.getElementById('runCode');
+
+    runCode.contentDocument.body.innerHTML = codeHTML + codeJS + codeCSS;
+    runCode.contentWindow.eval(codeJS);
+
+    // add event listener to runCode
+    runCode.addEventListener('load', function() {
+        console.log('Loaded');
+    });
+
+    // add event listener to runCode
+    runCode.addEventListener('error', function() {
+        console.log('Error');
+    });
+
+  }
+  document.querySelector('#codeHTML').addEventListener('keyup', run);
+  document.querySelector('#codeCSS').addEventListener('keyup', run);
+  document.querySelector('#codeJS').addEventListener('keyup', run);
