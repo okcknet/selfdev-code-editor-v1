99b0abb3c38afaed2afff2f52e9f895be6bc7541
Oguz Kaan C Kilinc <okckilinc@gmail.com>
2022-05-21 13:24:33 +0300

    saved to local storage

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log('Hello World');
 
+const saveCode = document.getElementById('saveCode');
+
 function run() {
     console.log('Running');
 
@@ -25,3 +27,43 @@ function run() {
   document.querySelector('#codeHTML').addEventListener('keyup', run);
   document.querySelector('#codeCSS').addEventListener('keyup', run);
   document.querySelector('#codeJS').addEventListener('keyup', run);
+
+  // save code to local storage with confirmation dialog
+  saveCode.addEventListener('click', function() {
+    let codeHTML  = document.getElementById('codeHTML').value;
+    let codeJS    = document.getElementById('codeJS').value;
+    let codeCSS   = document.getElementById('codeCSS').value;
+    let codeDate  = new Date();
+
+    let code = {
+        html: codeHTML,
+        js: codeJS,
+        css: codeCSS,
+        date: codeDate
+    };
+
+    if (confirm('Save code?')) {
+        localStorage.setItem('code', JSON.stringify(code));
+    }
+  });
+
+
+
+
+  // saveCode.addEventListener('click', function() {
+  //   let codeHTML  = document.getElementById('codeHTML').value;
+  //   let codeJS    = document.getElementById('codeJS').value;
+  //   let codeCSS   = document.getElementById('codeCSS').value;
+  //   let code      = codeHTML + codeJS + codeCSS;
+  //   let blob      = new Blob([code], {type: "text/plain;charset=utf-8"});
+  //   saveAs(blob, "code.html");
+  // });
+
+  // function saveAs(blob, fileName) {
+  //   let url = URL.createObjectURL(blob);
+  //   let a = document.createElement('a');
+  //   a.href = url;
+  //   a.download = fileName;
+  //   a.click();
+  //   URL.revokeObjectURL(url);
+  // }
